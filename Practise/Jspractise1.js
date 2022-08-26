b96bdbc098c42e5ab712de6681f1d57824c82aa6
Arjhun <arjhun7204@gmail.com>
2022-08-26 11:08:51 +0530

    Initail commit

---
+++ Practise/Jspractise1.js
@@ -0,0 +1,37 @@
+// const arr= [10,20,30]
+// let[a,b,c]= arr
+// console.log(b);
+
+// [b,a]=[a,b]
+// console.log(b);
+
+const movielist=[
+    {
+    name: "RRR",
+    poster:
+    "https://englishtribuneimages.blob.core.windows.net/gallary-content/2021/6/Desk/2021_6$largeimg_977224513.JPG",
+    rating: 8.8,
+    summary:
+    "RRR is an upcoming Indian Telugu-language period action drama film directed by S. S. Rajamouli, and produced by D. V. V. Danayya of DVV Entertainments.",
+    
+    },
+    {
+    name: "Iron man 2",
+    poster:
+    "https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_FMjpg_UX1000_.jpg",
+    rating: 7,
+    summary:
+    "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+    
+    }]
+    // {
+    // name: "No Country for Old Men",
+    // poster:
+    // "https://upload.wikimedia.org/wikipedia/en/8/8b/No_Country_for_Old_Men_poster.jpg",
+    // rating: 8.1,
+    // summary:
+    // "A hunter's life takes a drastic turn when he discovers two million dollars while strolling through the aftermath of a drug deal. He is then pursued by a psychopathic killer who wants the money.",
+    
+    // },
+
+
